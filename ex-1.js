712e0a340b440da2e7fe26f9300e655524ba43a1
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-05-16 13:55:38 +0700

    feat: ex-loop test

--- ex-1.js
+++ ex-1.js
@@ -2,3 +2,8 @@
 let scores = [89, 70, 60, 50, 100, 71];
 
 // Start coding here
+for (let index = 0; index < scores.length; index++) {
+  scores[index] = scores[index] - 10;
+}
+
+console.log(scores);
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,6 @@
 let companyName = "TechUp";
 
 // Start coding here
+for (let index = 0; index < companyName.length; index++) {
+  console.log(`Number ${index} character is ${companyName[index]}`);
+}
